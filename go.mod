ccb726672e57f1eca60466d10ad5a46eb2498b5b
zhengshude <123456789@qq.com>
2021-04-09 11:20:24 +0800

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module session.imchuncai.com
+
+go 1.15
+
+require (
+	github.com/go-redis/redis/v8 v8.8.0
+	github.com/kr/pretty v0.1.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+)
---
+++ session.go
@@ -0,0 +1,97 @@
+package session
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/binary"
+	"io"
+	"strings"
+	"time"
+
+	"github.com/go-redis/redis/v8"
+)
+
+type session struct {
+	redis     *redis.Client
+	timeout   time.Duration
+	keyPrefix string
+	incrKey   string
+	dataFunc  SessionDataBuilder
+}
+
+func NewSession(redis *redis.Client, timeout time.Duration, keyPrefix string, dataFunc SessionDataBuilder) session {
+	return session{redis, timeout, keyPrefix, keyPrefix + "incr", dataFunc}
+}
+
+type SessionDataBuilder interface {
+	New(sessionID string) string
+	Unmarshal(data string, v interface{}) error
+	Marshal(v interface{}) (string, error)
+}
+
+func (s session) valid(id string) bool {
+	return strings.HasPrefix(id, s.keyPrefix) && !strings.HasPrefix(id, s.incrKey)
+}
+
+func (s session) Get(id string, sessionData interface{}, ttl time.Duration) (newID string, err error) {
+	if s.valid(id) {
+		var v, err = s.get(id, ttl)
+		if err == nil {
+			return id, s.dataFunc.Unmarshal(v, &sessionData)
+		}
+		if err != redis.Nil {
+			return "", err
+		}
+	}
+	id, err = s.newSessionID()
+	if err != nil {
+		return "", err
+	}
+	return id, s.Set(id, s.dataFunc.New(id), ttl)
+}
+
+func (s session) newSessionID() (string, error) {
+	var incr, err = s.incr()
+	if err != nil {
+		return "", err
+	}
+	var b = make([]byte, 64+8+4)
+	io.ReadFull(rand.Reader, b[:64])
+	binary.BigEndian.PutUint64(b[64:64+8], uint64(time.Now().UnixNano()))
+	binary.BigEndian.PutUint32(b[64+8:], uint32(incr))
+	return base64.URLEncoding.EncodeToString(append([]byte(s.keyPrefix), b...)), nil
+}
+
+func (s session) getTimeoutCtx() (context.Context, context.CancelFunc) {
+	return context.WithTimeout(context.Background(), s.timeout)
+}
+
+func (s session) Set(key string, val string, ttl time.Duration) error {
+	var ctx, cancel = s.getTimeoutCtx()
+	defer cancel()
+	return s.redis.SetEX(ctx, key, val, ttl).Err()
+}
+
+func (s session) get(key string, ddl time.Duration) (string, error) {
+	var ctx, cancel = s.getTimeoutCtx()
+	defer cancel()
+	const script = "redis.call('expire',KEYS[1],ARGV[1]) return redis.call('get',KEYS[1])"
+	return s.redis.Eval(ctx, script, []string{s.incrKey}, int(ddl.Seconds())).Text()
+}
+
+func (s session) incr() (int, error) {
+	var ctx, cancel = s.getTimeoutCtx()
+	defer cancel()
+	const script = "local v=redis.call('incr',KEYS[1]) if v>4000000000 then redis.call('del',KEYS[1]) end return v"
+	return s.redis.Eval(ctx, script, []string{s.incrKey}, nil).Int()
+}
+
+func (s session) Del(key string) error {
+	if !s.valid(key) {
+		return nil
+	}
+	var ctx, cancel = s.getTimeoutCtx()
+	defer cancel()
+	return s.redis.Del(ctx, key).Err()
+}
